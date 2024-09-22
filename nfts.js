083af7e6cfbbc23918d74c87d33e84c588686cce
Ragini009 <132693621+Ragini009@users.noreply.github.com>
2024-09-22 13:17:18 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "this is my krishna",
+    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+  },
+  {
+    name: "1",
+    description: "this is my krishna",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, //change
+  {
+    name: "2",
+    description: "this is my krishna",
+    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+  },
+  {
+    name: "3",
+    description: "this is my krishna",
+    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+  },
+  {
+    name: "4",
+    description: "this is my krishna.",
+    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+  },
+];
+
+module.exports = nfts;
